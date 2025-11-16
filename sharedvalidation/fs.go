4691b8ed73208714a102e64310ea897415cdcda6
calumjones311 <calumjones@outlook.com>
2025-11-16 13:01:50 +0000

    Working on validators.

--- sharedvalidation/fs.go
+++ sharedvalidation/fs.go
@@ -2,8 +2,10 @@
 package sharedvalidation
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/TubarrApp/gocommon/sharedconsts"
 )
@@ -11,58 +13,74 @@ import (
 // ValidateDirectory validates that the directory exists, else creates it if desired.
 func ValidateDirectory(dir string, createIfNotFound bool) (os.FileInfo, error) {
 	// Check directory existence
-	dirInfo, err := os.Stat(dir)
-	switch {
-	case err == nil: // If err IS nil
-		if !dirInfo.IsDir() {
-			return dirInfo, fmt.Errorf("path %q is a file, not a directory", dir)
-		}
-		return dirInfo, nil
+	dir = filepath.Clean(dir)
 
-	case os.IsNotExist(err):
-		// path does not exist
-		if createIfNotFound {
-			if err := os.MkdirAll(dir, sharedconsts.PermsGenericDir); err != nil {
-				return nil, fmt.Errorf("directory %q does not exist and failed to create: %w", dir, err)
-			}
-			if dirInfo, err = os.Stat(dir); err != nil { // re-stat to get correct FileInfo
-				return dirInfo, fmt.Errorf("failed to stat %q", dir)
-			}
-			return dirInfo, nil
+	// Stat path
+	info, err := os.Stat(dir)
+	if err == nil { // Err IS nil
+		if !info.IsDir() {
+			return nil, fmt.Errorf("path %q is a file, not a directory", dir)
 		}
-		return nil, fmt.Errorf("directory %q does not exist", dir)
+		return info, nil
+	}
 
-	default:
-		// other error
+	// Error other than non-existence
+	if !errors.Is(err, os.ErrNotExist) {
 		return nil, fmt.Errorf("failed to stat directory %q: %w", dir, err)
 	}
+
+	// Does not exist, should not create
+	if !createIfNotFound {
+		return nil, fmt.Errorf("directory %q does not exist", dir)
+	}
+
+	// Generate new directories
+	if err := os.MkdirAll(dir, sharedconsts.PermsGenericDir); err != nil {
+		return nil, fmt.Errorf("directory %q does not exist and failed to create: %w", dir, err)
+	}
+
+	// Stat newly generated directory
+	info, err = os.Stat(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to stat %q", dir)
+	}
+	return info, nil
 }
 
 // ValidateFile validates that the file exists, else creates it if desired.
-func ValidateFile(f string, createIfNotFound bool) (os.FileInfo, error) {
-	fileInfo, err := os.Stat(f)
-	if err != nil {
-		if os.IsNotExist(err) {
-			if createIfNotFound {
-				file, err := os.Create(f)
-				if err != nil {
-					return nil, fmt.Errorf("file %q does not exist and failed to create: %w", f, err)
-				}
-				file.Close()
-				// Re-stat to get correct FileInfo
-				if fileInfo, err = os.Stat(f); err != nil {
-					return nil, fmt.Errorf("failed to stat %q after creation", f)
-				}
-				return fileInfo, nil
-			}
-			return nil, fmt.Errorf("file %q does not exist", f)
+func ValidateFile(path string, createIfNotFound bool) (os.FileInfo, error) {
+	path = filepath.Clean(path)
+
+	// Stat path
+	info, err := os.Stat(path)
+	if err == nil { // Err IS nil
+		if info.IsDir() {
+			return nil, fmt.Errorf("path %q is a directory, not a file", path)
 		}
-		return nil, fmt.Errorf("failed to stat file %q: %w", f, err)
+		return info, nil
 	}
 
-	if fileInfo.IsDir() {
-		return fileInfo, fmt.Errorf("path %q is a directory, not a file", f)
+	// Error other than non-existence
+	if !errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf("failed to stat file %q: %w", path, err)
 	}
 
-	return fileInfo, nil
+	// Does not exist, should not create
+	if !createIfNotFound {
+		return nil, fmt.Errorf("file %q does not exist", path)
+	}
+
+	// Generate new file (must close after os.Create())
+	file, err := os.Create(path)
+	if err != nil {
+		return nil, fmt.Errorf("file %q does not exist and failed to create: %w", path, err)
+	}
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			fmt.Fprintf(os.Stderr, "Failed to close file %q: %v", file.Name(), closeErr)
+		}
+	}()
+
+	// Return info and nil/err
+	return os.Stat(path)
 }
