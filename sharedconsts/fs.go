db062b11fd01ffff989c3df9b35606d74d233264
calumjones311 <calumjones@outlook.com>
2025-11-17 15:37:37 +0000

    Added rename flag grab.

--- sharedconsts/fs.go
+++ sharedconsts/fs.go
@@ -2,15 +2,15 @@ package sharedconsts
 
 // Rename styles.
 const (
+	RenameFixesOnly   = "fixes-only"
 	RenameSkip        = "skip"
 	RenameSpaces      = "spaces"
 	RenameUnderscores = "underscores"
-	RenameFixesOnly   = "fixes-only"
 )
 
 var ValidRenameFlags = map[string]bool{
+	RenameFixesOnly:   true,
 	RenameSkip:        true,
 	RenameSpaces:      true,
 	RenameUnderscores: true,
-	RenameFixesOnly:   true,
 }
