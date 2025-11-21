e4237264e7a94b497c6b519862c4cde0fb40bad8
calumjones311 <calumjones@outlook.com>
2025-11-21 02:14:42 +0000

    Added container metatag keys for Matroska.

--- sharedtags/container_metatags.go
+++ sharedtags/container_metatags.go
@@ -1,18 +1,6 @@
 // Package sharedtags holds tag information for things like JSON fields, and container metatags.
 package sharedtags
 
-// ISOBMFF container metatag keys.
-const (
-	ISOArtist       = "artist"
-	ISOComment      = "comment"
-	ISOComposer     = "composer"
-	ISOCreationTime = "creation_time"
-	ISODate         = "date"
-	ISODescription  = "description"
-	ISOSynopsis     = "synopsis"
-	ISOTitle        = "title"
-)
-
 // ASF container metatag keys.
 const (
 	ASFArtist              = "WM/AlbumArtist"
@@ -24,22 +12,6 @@ const (
 	ASFSubTitleDescription = "WM/SubTitleDescription"
 	ASFTitle               = "Title"
 	ASFYear                = "WM/Year"
-)
-
-// Ogg container metatag keys.
-const (
-	OggArtist      = "ARTIST"
-	OggComposer    = "COMPOSER"
-	OggDate        = "DATE"
-	OggDescription = "DESCRIPTION"
-	OggPerformer   = "PERFORMER"
-	OggSummary     = "SUMMARY"
-	OggTitle       = "TITLE"
-)
-
-// FLV container metatags keys.
-const (
-	FLVCreationDate = "creationdate"
 )
 
 // AVI container metatag keys.
@@ -54,15 +26,60 @@ const (
 	AVIYear        = "YEAR"
 )
 
-// RealMedia container metatag keys.
+// FLV container metatags keys.
 const (
-	RMAuthor  = "Author"
-	RMComment = "Comment"
-	RMTitle   = "Title"
+	FLVCreationDate = "creationdate"
+)
+
+// ISOBMFF container metatag keys.
+const (
+	ISOArtist       = "artist"
+	ISOComment      = "comment"
+	ISOComposer     = "composer"
+	ISOCreationTime = "creation_time"
+	ISODate         = "date"
+	ISODescription  = "description"
+	ISOSynopsis     = "synopsis"
+	ISOTitle        = "title"
+)
+
+// Matroska container metatag keys.
+const (
+	MatroskaArtist        = "ARTIST"
+	MatroskaComposer      = "COMPOSER"
+	MatroskaDateEncoded   = "DATE_ENCODED"
+	MatroskaDateReleased  = "DATE_RELEASED"
+	MatroskaDescription   = "DESCRIPTION"
+	MatroskaDirector      = "DIRECTOR"
+	MatroskaLeadPerformer = "LEAD_PERFORMER"
+	MatroskaPerformer     = "PERFORMER"
+	MatroskaProducer      = "PRODUCER"
+	MatroskaSubject       = "SUBJECT"
+	MatroskaSummary       = "SUMMARY"
+	MatroskaSynopsis      = "SYNOPSIS"
+	MatroskaTitle         = "TITLE"
 )
 
 // MPEG-TS container metatag keys.
 const (
 	TSServiceName     = "service_name"
 	TSServiceProvider = "service_provider"
+)
+
+// Ogg container metatag keys.
+const (
+	OggArtist      = "ARTIST"
+	OggComposer    = "COMPOSER"
+	OggDate        = "DATE"
+	OggDescription = "DESCRIPTION"
+	OggPerformer   = "PERFORMER"
+	OggSummary     = "SUMMARY"
+	OggTitle       = "TITLE"
+)
+
+// RealMedia container metatag keys.
+const (
+	RMAuthor  = "Author"
+	RMComment = "Comment"
+	RMTitle   = "Title"
 )
