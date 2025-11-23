3bfb314c4c305d6b000ecf2bcaa8fd478cfe0fb3
calumjones311 <calumjones@outlook.com>
2025-11-23 07:11:18 +0000

    Add shared constants for various operations in ops.go.

---
+++ sharedconsts/ops.go
@@ -0,0 +1,32 @@
+package sharedconsts
+
+// Operations for various features.
+const (
+	// Contains or omits.
+	OpContains = "contains"
+	OpOmits    = "omits"
+
+	// Musts and anys.
+	OpMust = "must"
+	OpAny  = "any"
+
+	// Copy/paste.
+	OpCopyTo    = "copy-to"
+	OpPasteFrom = "pate-from"
+
+	// Date tag operations.
+	OpDateTag       = "date-tag"
+	OpDeleteDateTag = "delete-date-tag"
+
+	// Replacement operations.
+	OpReplace       = "replace"
+	OpReplaceSuffix = "replace-suffix"
+	OpReplacePrefix = "replace-prefix"
+
+	// Append and suffix.
+	OpAppend = "append"
+	OpSuffix = "suffix"
+
+	// Set.
+	OpSet = "set"
+)
